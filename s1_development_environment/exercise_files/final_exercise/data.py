0ce14628f5f362abaefcc8e950d3b3ca81bea918
michallehwark <michallehwark511@gmail.com>
2023-01-05 20:41:10 +0100

    sdasdPlease enter the commit message for your changes. Lines starting

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,9 +1,50 @@
+#import random
 import numpy as np
 import torch
+from torch.utils.data import DataLoader, TensorDataset
 
+def mnist(shuffle=True, batch_size=64):
+    
+    train_images = []
+    train_labels = []
+    
+    for i in range(5):
+        data = np.load(f"C:\\Users\\micha\\OneDrive\\Pulpit\\DTU\\02476\\dtu_mlops\\data\\corruptmnist\\train_{i}.npz")
+        [train_images.append(img) for img in data["images"]]
+        [train_labels.append(label) for label in data["labels"]]
 
-def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+    train_images = np.array(train_images)
+    # reshape to  (n_immgs, channels, pixels, pixels)
+    train_images = train_images.reshape(train_images.shape[0], 1, 28, 28)
+    train_labels = np.array(train_labels)
+
+    train = [
+        torch.from_numpy(train_images).type(torch.float32),
+        torch.from_numpy(train_labels).long(),
+    ]
+
+    test_images = []
+    test_labels = []
+    data = np.load("C:\\Users\\micha\\OneDrive\\Pulpit\\DTU\\02476\\dtu_mlops\\data\\corruptmnist\\test.npz")
+    [test_images.append(img) for img in data["images"]]
+    [test_labels.append(label) for label in data["labels"]]
+
+    test_images = np.array(test_images)
+    test_images = test_images.reshape(test_images.shape[0], 1, 28, 28)
+    test_labels = np.array(test_labels)
+    test = [
+        torch.tensor(test_images).type(torch.float32),
+        torch.tensor(test_labels).long(),
+    ]
+
+    train_dataset = TensorDataset(train[0], train[1])  # create your datset
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=shuffle
+    )  # create your dataloader
+
+    test_dataset = TensorDataset(test[0], test[1])  # create your datset
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size
+    )  # create your dataloader
+
+    return train_loader, test_loader
--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,6 +1,22 @@
 from torch import nn
+import torch.nn.functional as F
 
 
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
+
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        #output
+        self.fc3 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        # Pass the input tensor through each of our operations
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = nn.LogSoftmax(dim=1)
+        
+        return x
